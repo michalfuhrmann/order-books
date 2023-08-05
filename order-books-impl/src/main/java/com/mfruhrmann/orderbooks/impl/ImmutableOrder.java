8b2adcc501b024090f06940067d302734245ac6c
Michal <torosmg@gmail.com>
2023-08-05 16:04:44 +0200

    typo fix

---
+++ order-books-impl/src/main/java/com/mfruhrmann/orderbooks/impl/ImmutableOrder.java
@@ -0,0 +1,9 @@
+package com.mfruhrmann.orderbooks.impl;
+
+import com.mfruhrmann.orderbooks.api.OrderBook;
+
+import java.time.Instant;
+
+public record ImmutableOrder(String id, OrderBook.Side side, OrderBook.OrderType type, Instant ts, double price,
+                             int size) implements OrderBook.Order {
+}
